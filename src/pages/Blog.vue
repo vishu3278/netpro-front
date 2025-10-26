a74d2be140c9e01b6a5f4123cfffb17c7be1930d
GP522 <golupraja96@gmail.com>
2025-10-26 12:53:45 +0530

    blog

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,48 +1,222 @@
 <template>
 	<section class="w-full bg-white pt-50 pb-10 blogsection">
-		<div class="max-w-6xl mx-auto px-6">
+		<div class="container mx-auto">
+			<div class="">
 
-			<!-- Title -->
-			<h1 class=" mb-8">
-				Latest from the blogs
-			</h1>
+				<!-- Title -->
+				<h1 class=" mb-8">
+					Latest from the blogs
+				</h1>
 
-			<!-- Blog Banner -->
-			<div class="relative rounded-2xl overflow-hidden h-100 md:h-100 bg-cover bg-center"
-				style="background-image: url('/public/Blog/blog-banner.jpg');">
+				<!-- Blog Banner -->
+				<div class="relative rounded-2xl overflow-hidden h-100 md:h-100 bg-cover bg-center"
+					style="background-image: url('/public/Blog/blog-banner.jpg');">
 
-				<!-- Content Overlay -->
-				<div class="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent flex flex-col justify-between px-16 pt-16 md:py-6">
+					<!-- Content Overlay -->
+					<div
+						class="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent flex flex-col justify-between px-15 pt-20 md:py-8">
 
-					<!-- Featured Tag -->
-					<span class=" w-fit mb-4">
-						Featured
-					</span>
+						<!-- Featured Tag -->
+						<span class=" w-fit mb-4">
+							Featured
+						</span>
 
-					<div class="solutions-hd">
-						<!-- Title -->
-					<h2 class="mb-3">
-						Staying ahead in the game <br> via Smart IT Solutions
-					</h2>
+						<div class="solutions-hd">
+							<!-- Title -->
+							<h2 class="mb-3">
+								Staying ahead in the game <br> via Smart IT Solutions
+							</h2>
 
-					<!-- Description -->
-					<p class="max-w-md mb-6">
-						For exclusive stories we have brought to you an insider story of companies that has made tech
-						disucssion.
-					</p>
+							<!-- Description -->
+							<p class="max-w-md mb-6">
+								For exclusive stories we have brought to you an insider story of companies that has made
+								tech
+								disucssion.
+							</p>
 
-					<!-- Button - Read More -->
-					<div>
-						<a href="#" class="">
-							Read More <img src="/public/Blog/arrow-right.svg" alt="">
-						</a>
+							<!-- Button - Read More -->
+							<div>
+								<a href="#" class="">
+									Read More <img src="/public/Blog/arrow-right.svg" alt="">
+								</a>
+							</div>
+						</div>
+
 					</div>
-					</div>
-
 				</div>
 			</div>
 		</div>
 	</section><!--blogsection-->
+
+
+
+	<section class="w-full bg-white pt-16 pb-20  blogpost-section">
+		<div class="container mx-auto">
+
+			<div class="">
+
+				<!-- Filters + View All -->
+				<div class="flex justify-between items-center mb-10 border-b border-[#D7D7D7] pb-10">
+					<div class="flex gap-3 flex-wrap">
+						<button class="navtaps  bg-gray-900 !text-white">All Topics</button>
+						<button class="navtaps">Blogs</button>
+						<button class="navtaps">News</button>
+						<button class="navtaps">Events</button>
+						<button class="navtaps ">Resources</button>
+					</div>
+
+					<a href="#" class="text-sm text-gray-600 hover:underline">
+						View All
+					</a>
+				</div>
+
+				<!-- Blog Grid -->
+				<div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post1.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								Glimpse of NetProphets at Intersec Italy 2024
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post2.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								The power of secure system for access management
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post3.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								BigCommerce vs Shopify: Which Platform Is Right for You in 2025?
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post1.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								Glimpse of NetProphets at Intersec Italy 2024
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post2.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								The power of secure system for access management
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post3.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								BigCommerce vs Shopify: Which Platform Is Right for You in 2025?
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+
+
+
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post1.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								Glimpse of NetProphets at Intersec Italy 2024
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post2.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								The power of secure system for access management
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+					<!-- Blog Card -->
+					<div class="rounded-2xl overflow-hidden border border-gray-200 bg-white relative">
+						<img src="/public/Blog/blog-post3.jpg" class="w-full object-cover" alt="">
+						<div class="p-6">
+							<span class="py-6">15 May 2024</span>
+							<p class="mb-6">
+								BigCommerce vs Shopify: Which Platform Is Right for You in 2025?
+							</p>
+							<a href="#" class="text-sm inline-flex items-center gap-2">
+								Read More <img src="/public/Blog/Read-arrow.svg" alt="">
+							</a>
+						</div>
+					</div>
+
+				</div>
+
+				<!-- Load More -->
+				<div class="text-center mt-10">
+					<button type="button" class="btn text-sm hover:bg-gray-100">
+						Load More
+					</button>
+				</div>
+
+			</div>
+		</div>
+	</section><!--blogpost-section-->
+
+
 
 </template>
 
@@ -60,44 +234,115 @@ h1 {
 	letter-spacing: -1.8px;
 }
 
-span {
-	display: inline-flex;
-	padding: 6px 20px;
-	justify-content: center;
-	align-items: center;
-	gap: 8px;
-	border-radius: 55px;
-	border: 1px solid var(--White, #FFF);
-	color: #FFF;
-	font-size: 14px;
-	font-weight: 600;
-	line-height: 30px;
+.blogsection {
+
+	span {
+		display: inline-flex;
+		padding: 6px 20px;
+		justify-content: center;
+		align-items: center;
+		gap: 8px;
+		border-radius: 55px;
+		border: 1px solid var(--White, #FFF);
+		color: #FFF;
+		font-size: 14px;
+		font-weight: 600;
+		line-height: 30px;
+	}
+
+	h2 {
+		color: #fff;
+		font-size: 40px;
+		font-weight: 600;
+		line-height: 45px;
+		letter-spacing: -1.2px;
+	}
+
+	p {
+		color: #FFF;
+		font-size: 14px;
+		font-weight: 300;
+		line-height: 20px;
+		letter-spacing: -0.42px;
+	}
+
+	a {
+		color: #fff;
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 30px;
+		letter-spacing: -0.48px;
+		display: flex;
+		align-items: center;
+		gap: 0px 10px;
+	}
+
 }
 
-h2 {
-	color: #fff;
-	font-size: 40px;
-	font-weight: 600;
-	line-height: 45px;
-	letter-spacing: -1.2px;
-}
+.blogpost-section {
 
-p {
-	color: #FFF;
-	font-size: 14px;
-	font-weight: 300;
-	line-height: 20px;
-	letter-spacing: -0.42px;
-}
+	span {
+		color: #fff;
+		font-size: 14px;
+		font-weight: 400;
+		line-height: 30px;
+		letter-spacing: -0.42px;
+		display: block;
+		position: absolute;
+		top: 0px;
+	}
 
-a {
-	color: #fff;
-	font-size: 16px;
-	font-weight: 400;
-	line-height: 30px;
-	letter-spacing: -0.48px;
-	display: flex;
-	align-items: center;
-	gap: 0px 10px;
+	p {
+		color: $rich-black;
+		font-size: 24px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: 38px;
+		letter-spacing: -0.72px;
+	}
+
+	a {
+		color: $rich-black;
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 30px;
+		letter-spacing: -0.48px;
+	}
+
+	button.btn {
+		display: inline-flex;
+		height: 55px;
+		padding: 6px 20px;
+		justify-content: center;
+		align-items: center;
+		gap: 8px;
+		border-radius: 55px;
+		background: var(--Rich-Black, #121212);
+		color: #FFF;
+		font-size: 18px;
+		font-weight: 400;
+		line-height: 30px;
+		letter-spacing: -0.54px;
+		cursor: pointer;
+	}
+
+
+	.navtaps {
+		color: #5F5F5F;
+		font-size: 18px;
+		font-weight: 400;
+		line-height: 30px;
+		letter-spacing: -0.54px;
+		border-radius: 55px;
+		border: 1px solid var(--Stroke, #D7D7D7);
+		display: inline-flex;
+		height: 55px;
+		padding: 6px 20px;
+		justify-content: center;
+		align-items: center;
+		gap: 8px;
+	}
+
+
 }
 </style>
