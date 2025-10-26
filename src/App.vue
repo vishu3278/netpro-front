ea61c8f05625b233c333854b1db06d29b4ab5879
vishu3278 <vishu3278@gmail.com>
2025-10-26 16:07:58 +0530

    env variable

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,8 @@ onMounted(() => {
         effects: true, // looks for data-speed and data-lag attributes on elements
         // smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
     });
+    console.log('Base URL:', import.meta.env.VITE_APP_BASE_URL)
+
 })
 
 </script>
